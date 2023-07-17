9ccfe8e1f507e324b0ccc20299f8b173959aaf5f
Artem <a.novikov@acsaudiovisual.com>
2023-07-17 13:01:10 +0200

    Gitting project

---
+++ app/Models/Company.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Company extends Model
+{
+    use HasFactory;
+
+
+    public static function boot() {
+        parent::boot();
+
+        
+
+        static::saving(function ($model) {
+
+            $model->logo = str_replace("\\", "/", $model->logo);
+           
+        });
+
+
+
+    }
+
+}
